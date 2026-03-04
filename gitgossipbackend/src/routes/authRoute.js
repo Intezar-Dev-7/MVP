4248f7d3b54fc25d9f76158305ed4f1aa702e402
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2026-03-04 09:59:36 +0530

    nothing fixed

--- gitgossipbackend/src/routes/authRoute.js
+++ gitgossipbackend/src/routes/authRoute.js
@@ -6,7 +6,6 @@ import { saveUser } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
-console.log("Auth Route hi");
 authRouter.post("/saveUser", verifyFirebaseToken, saveUser);
 
 export default authRouter;
--- gitgossipfrontend/lib/core/config/api.dart
+++ gitgossipfrontend/lib/core/config/api.dart
@@ -1,2 +1,2 @@
 // const String baseUrl = "http://10.200.187.72:3000";
-const String baseUrl = "http://192.168.1.41:3000";
+const String baseUrl = "http://192.168.1.46:3000";
